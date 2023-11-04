89f3c25c82bb1c0940c4c2a4b8ad949b72fb471f
EwertonLima2002 <ewerton.lima860@sptech.school>
2023-11-03 21:32:58 -0300

    :sparkles: feat: adicionado integração com o slack

--- Jar Alpaca Solutions/v2-jar-alpaca-solutions/src/main/java/dados/Slack.java
+++ Jar Alpaca Solutions/v2-jar-alpaca-solutions/src/main/java/dados/Slack.java
@@ -1,4 +1,32 @@
 package dados;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Diego Brito <diego.lima@bandtec.com.br>
+ */
 public class Slack {
+
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+
+    public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder(
+                        URI.create(URL))
+                .header("accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(String.format("Status: %s", response.statusCode()));
+        System.out.println(String.format("Response: %s", response.body()));
+    }
 }
