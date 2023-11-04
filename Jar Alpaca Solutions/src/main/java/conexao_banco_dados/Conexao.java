52315817376e5e161c23c1c4144ffa9076116cef
EwertonLima2002 <ewerton.lima860@sptech.school>
2023-11-04 19:36:13 -0300

    Jar fazendo select com determinado tipo de usuario

---
+++ Jar Alpaca Solutions/src/main/java/conexao_banco_dados/Conexao.java
@@ -0,0 +1,36 @@
+package conexao_banco_dados;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Conexao {
+    private JdbcTemplate conexaoDoBanco;
+
+    public Conexao() {
+        BasicDataSource dataSource = new BasicDataSource();
+        /*
+             Exemplo de driverClassName:
+                com.mysql.cj.jdbc.Driver <- EXEMPLO PARA MYSQL
+                com.microsoft.sqlserver.jdbc.SQLServerDriver <- EXEMPLO PARA SQL SERVER
+        */
+
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        /*
+             Exemplo de string de conexões:
+                jdbc:mysql://localhost:3306/mydb <- EXEMPLO PARA MYSQL
+                jdbc:sqlserver://localhost:1433;database=mydb <- EXEMPLO PARA SQL SERVER
+        */
+        dataSource.setUrl("jdbc:mysql://localhost:3306/AlpacaDB");
+        dataSource.setUsername("aluno");
+        dataSource.setPassword("aluno");
+
+        conexaoDoBanco = new JdbcTemplate(dataSource);
+    }
+
+
+    public JdbcTemplate getConexaoDoBanco(){
+        return  conexaoDoBanco;
+    }
+
+}
